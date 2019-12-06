834323834d896acbaed4b44402e883a6a5f82759
33180770 <koster.k2001@gmail.com>
2019-12-06 13:26:01 +0100

    useless commit

--- klean.py
+++ klean.py
@@ -1,13 +1,2 @@
-from datetime import datetime
-import glob
-import json
-import os
-
-# with open('config.json') as config:
-#     config_c = json.load(config)
-
-os.chdir("DelTest")
-for file in glob.glob():
-    print(file)
 
 
