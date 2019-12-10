e92742aea2b602a23a30803eb1f0f7ee1d9cdd36
33180770 <koster.k2001@gmail.com>
2019-12-10 16:13:07 +0100

    + kleine for loopjes

--- klean.py
+++ klean.py
@@ -13,11 +13,17 @@ for file in sorted(os.listdir("files")):
 # os.listdir is hier al gesorteerd
 
 
-# Haal de datum uit de string(filename).
+# haal de datum uit de string(filename).
 def parse_date(filename):
     """
         haalt de datum uit de filename
     """
-    date_parse = {filename: datetime.strptime(filename.split('+')[1].replace("%3A", ":").replace(";", " "), "%Y-%m-%d "
-                                                                                                            "%H:%M:%S")}
+    date_parse = datetime.strptime(filename, "%Y-%m-%d%" "H:%M:%S")
     return date_parse
+
+
+# haalt de values uit de dictionary en parsed de date uit de namen
+for db_name in d:
+    for f_name in d.keys():
+    #     values_string = str(f_name).split("+")[1].replace(';', '').replace('%3A', ':').split("]")[1]
+    # print(parse_date(values_string))
