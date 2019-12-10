f7e88208138ec82aa8f61bd2919c1ef888a3db82
JesperBorst <125890@student.drenthecollege.nl>
2019-12-10 13:57:20 +0100

    Merge branch 'master' of C:\Users\jtbor\PycharmProjects\Klean with conflicts.

--- klean.py
+++ klean.py
@@ -1,20 +1,5 @@
 import os
 from datetime import datetime
 
-d = {}
-
-for file in sorted(os.listdir("files")):
-    naam = file.split('+')[0]
-    if naam not in d:
-        d[naam] = []
-    d[naam].append(file)
-
-
-# Haal de datum uit de string(filename).
-def parse_date(filename):
-    """
-        haalt de datum uit de filename
-    """
-    date_parse = {filename: datetime.strptime(filename.split('+')[1].replace("%3A", ":").replace(";", " "), "%Y-%m-%d "
-                                                                                                            "%H:%M:%S")}
-    return date_parse
+sorted(os.listdir("/files"))
+print(os.listdir())
