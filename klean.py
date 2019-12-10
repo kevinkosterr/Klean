64ffc91763874ce33948a59d1209c02ff4cd06f4
33180770 <koster.k2001@gmail.com>
2019-12-10 13:57:04 +0100

    + added commentary

--- klean.py
+++ klean.py
@@ -3,11 +3,14 @@ from datetime import datetime
 
 d = {}
 
+# sorteert de files en zet ze in een dictionary
 for file in sorted(os.listdir("files")):
     naam = file.split('+')[0]
+    # als de naam niet in de dictionary voorkomt, maak dan een sleutel en geef 'm een lege lijst
     if naam not in d:
         d[naam] = []
     d[naam].append(file)
+# os.listdir is hier al gesorteerd
 
 
 # Haal de datum uit de string(filename).
