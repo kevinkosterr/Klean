1cefb65bf685331bfc4d8db4b25dff0ffa6fa90f
33180770 <koster.k2001@gmail.com>
2019-12-10 10:57:09 +0100

    hehe

--- klean.py
+++ klean.py
@@ -1,35 +1,19 @@
 import os
-import json
 from datetime import datetime
 
-# 1. referentiedatum halen uit de filenames van de back-ups
-"""
-    alle files hebben een datum in de naam, dit word de referentiedatum. er vinden 168 back-ups per week plaats.
-"""
-
-config = open("config.json")
-c = json.load(config)
-ref_date = os.listdir(c['path'])[0]
+# Bepaal de referentiedatum
+'''
+    Haal alle bestanden op en bepaal de datum die bij die bestanden hoort door ze uit de naam te halen.
+'''
 
 
 def parse_date(filename):
-    '''
-        gets the date from a string
-    '''
+    """
+        haalt de datum uit een string
+    """
     date_parse = datetime.strptime(filename.split('+')[1].replace("%3A", ":").replace(";", " "), "%Y-%m-%d %H:%M:%S")
     return date_parse
 
 
 for file in os.listdir("files"):
-    print(file, end=' - ')
-    print(parse_date(file))
-
-# 2. het verwijderen vanaf die datum voor de tweede week
-"""
-    tussen deze back-ups mag maximaal 4,5 uur per keer zitten. van de 168 blijven er dus 42 over.
-"""
-
-# 3. bepaal het volgende startpunt
-"""
-    het volgende startpunt is waar de laatste week geëindigd is. 
-"""
+    parse_date(file)
