c4bbcbe03fc5b7f0ce604c270337af8b82fcecf7
JesperBorst <125890@student.drenthecollege.nl>
2019-12-10 11:28:14 +0100

    Pseudo Code Voorbeeld

--- klean.py
+++ klean.py
@@ -1,21 +1,23 @@
 import os
 from datetime import datetime
 
-# Bepaal de referentiedatum
+# Pseudo Code
 '''
-    Doorzoek de directory voor files.
+    Doorzoek de directory naar files.
         Sorteer de files.
             Pak de laatste file.
                 Haal de datum uit de string(filename).
 '''
+
+
+# Doorzoek de directory naar files.
+def dir_scan():
+    for file in os.listdir("files"):
+        print(parse_date(file))
+
 
 # Haal de datum uit de string(filename).
 def parse_date(filename):
     date_parse = {filename: datetime.strptime(filename.split('+')[1].replace("%3A", ":").replace(";", " "), "%Y-%m-%d "
                                                                                                             "%H:%M:%S")}
     return date_parse
-
-
-# Doorzoekt de directory voor files.
-for file in os.listdir("files"):
-    print(parse_date(file))
