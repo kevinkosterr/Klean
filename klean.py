92fb5547c5ef152fe1453c30e260691f047aaefb
33180770 <koster.k2001@gmail.com>
2019-12-10 12:19:46 +0100

    * code

--- klean.py
+++ klean.py
@@ -4,20 +4,23 @@ from datetime import datetime
 # Pseudo Code
 '''
     Doorzoek de directory naar files.
-        Sorteer de files.
-            Pak de laatste file.
-                Haal de datum uit de string(filename).
+        Maak een lijst met alle databases daarin.
+            Sorteer de files. (SET)
+                Pak de laatste file.
+                    Haal de datum uit de string(filename).
 '''
 
 
 # Doorzoek de directory naar files.
 def dir_scan():
     for file in os.listdir("files"):
-        print(parse_date(file))
-
+        iwannadie
 
 # Haal de datum uit de string(filename).
 def parse_date(filename):
     date_parse = {filename: datetime.strptime(filename.split('+')[1].replace("%3A", ":").replace(";", " "), "%Y-%m-%d "
                                                                                                             "%H:%M:%S")}
     return date_parse
+
+
+dir_scan()
