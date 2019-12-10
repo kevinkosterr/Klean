31870f6826d94bf1d00632867715a217bee8b28e
33180770 <koster.k2001@gmail.com>
2019-12-10 11:09:14 +0100

    * dictionary

--- klean.py
+++ klean.py
@@ -8,13 +8,15 @@ from datetime import datetime
             Haal de datum uit de string(filename).
 '''
 
+
 def parse_date(filename):
     """
         haalt de datum uit een string
     """
-    date_parse = datetime.strptime(filename.split('+')[1].replace("%3A", ":").replace(";", " "), "%Y-%m-%d %H:%M:%S")
+    date_parse = {filename: datetime.strptime(filename.split('+')[1].replace("%3A", ":").replace(";", " "), "%Y-%m-%d "
+                                                                                                            "%H:%M:%S")}
     return date_parse
 
 
 for file in os.listdir("files"):
-    parse_date(file)
+    print(parse_date(file))
