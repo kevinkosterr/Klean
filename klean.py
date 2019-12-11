fb86d4282d45d96d41d9473a6913dac1f6860fc6
33180770 <koster.k2001@gmail.com>
2019-12-11 13:13:53 +0100

    * extra test

--- klean.py
+++ klean.py
@@ -5,7 +5,7 @@ from datetime import datetime
 d = {}
 
 # sorteert de files en zet ze in een dictionary
-for file in sorted(os.listdir("files")):
+for file in sorted(os.listdir("files"), reverse=True):
     key_name = file.split('+')[0]
     # als de naam niet in de dictionary voorkomt, maak dan een sleutel en geef 'm een lege lijst
     if key_name not in d:
@@ -26,7 +26,9 @@ def parse_date(filename):
 # haalt de values uit de dictionary en parsed de date uit de namen
 # zoekt in de dictionary naar elke databasename
 for db_name in d:
+    print(db_name)
     # zoekt elke value in de keys
     for values in d[db_name]:
         values = str(values).split("+")[1]
         parsed_values = parse_date(values.replace(";", '').replace('%3A', ':'))
+        print(parsed_values)
