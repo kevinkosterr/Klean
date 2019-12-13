db4cf985e8612ceb5b144a2278d8d691e449f02e
33180770 <koster.k2001@gmail.com>
2019-12-13 11:00:23 +0100

    * hihi

--- klean.py
+++ klean.py
@@ -5,7 +5,8 @@ from datetime import datetime, timedelta
 d = {}
 
 # sorteert de files en zet ze in een dictionary
-for file in sorted(os.listdir("files"), reverse=True):
+# os.listdir("files")
+for file in sorted(open("filelist").read().split("\n"), reverse=True):
     key_name = file.split('+')[0]
     # als de naam niet in de dictionary voorkomt, maak dan een sleutel en geef 'm een lege lijst
     if key_name not in d:
@@ -23,13 +24,9 @@ def parse_date(filename):
     """
     # file_parsed = datetime.strptime(filename, "%Y-%m-%d%" "H:%M:%S")
     # return file_parsed
-    file_to_parse = str(filename).split("+")[1]
-    parsed_file = datetime.strptime(file_to_parse.replace(";", '').replace('%3A', ':'), "%Y-%m-%d%" "H:%M:%S")
+    file_to_parse = str(filename).split("+")[1].split(".")[0]
+    parsed_file = datetime.strptime(str(file_to_parse).replace(";", '').replace('%3A', ':'), "%Y-%m-%d%" "H:%M:%S")
     return parsed_file
-
-
-# de lijst waarin alle files komen te staan die verwijderd moeten worden
-kill_list = []
 
 
 def process_bucket(start_point, list_to_compare, hours):
@@ -37,16 +34,21 @@ def process_bucket(start_point, list_to_compare, hours):
     rekent het verschil tussen het startpunt en het volgende item, paramater hours geeft aan hoeveel uren
     er maximaal tussen elke back-up mag zitten.
     """
+    kill_list = []
     for item in list_to_compare:
         diff_start = parse_date(start_point) - parse_date(item)
         if diff_start < timedelta(hours=hours):
             kill_list.append(item)
         else:
             break
+    if not kill_list:
+        return []
     last = kill_list.pop()
     new_list = list_to_compare[list_to_compare.index(last):]
+    return kill_list
 
 
+kill_list = []
 for db_name in d.keys():
     bucket1 = []  # week 1
     bucket2 = []  # week 2
@@ -67,7 +69,10 @@ for db_name in d.keys():
         elif diff >= timedelta(days=85):
             bucket5.append(cursor)
 
-    process_bucket(bucket1[-1], bucket2, 4.5)
+    kill_list.extend(process_bucket(bucket1[-1], bucket2, 4.5))
+    kill_list.extend(process_bucket(bucket2[-1], bucket3, 12.5))
+    kill_list.extend(process_bucket(bucket3[-1], bucket4, 24))
+    kill_list.extend(process_bucket(bucket4[-1], bucket5, 168))
 
 # haalt de values uit de dictionary en parsed de date uit de namen
 # zoekt in de dictionary naar elke databasename
