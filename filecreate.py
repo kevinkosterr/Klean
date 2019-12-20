a04f346cae65a0dd36b9c75e697135f073a5b857
Kevin Koster <33180770+kevinkosterr@users.noreply.github.com>
2019-12-20 09:33:56 +0100

    Delete filecreate.py

--- filecreate.py
+++
@@ -1,4 +0,0 @@
-with open('filelist', 'r') as f:
-    for filename in f:
-        filename = str(filename).split("\n")[0]
-        c = open(f"C:\\Users\\koste\\PycharmProjects\\Klean\\files\\{filename}", 'x')
