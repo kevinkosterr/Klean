a6a1eab15bc6ef834a8747fea3754abdc44c0fae
33180770 <koster.k2001@gmail.com>
2020-01-14 10:39:24 +0100

    script fully works locally now

--- script.py
+++ script.py
@@ -1,60 +1,66 @@
-"""
-A script that deletes back-ups within a certain period of time.
-
-Certain parts of this script can be configured using the config.toml file.
-"""
 import toml
 import os
 from datetime import datetime, timedelta
+import cProfile
 
 
-def file_sorter():
-    """" Sorts the files of the directory and puts them in a dictionary
+# get a sorted list of files
+def get_sorted_files():
+    sorted_files = sorted(os.listdir(config.get('main').get('directory')), reverse=True)
+    # return a reverse sorted file list
+    return sorted_files
+
+
+def calc_diff_between_dates(filedate1, filedate2):
+    diff = get_file_date(filedate1) - get_file_date(filedate2)
+    return diff
+
+
+def get_files_per_db():
+    """Gets the files per database and puts them into a dictionary
+
+    :return: files_per_db: dictionary filled with files per database
     """
-    for filename in sorted(os.listdir(config.get('main').get('directory')), reverse=True):
+    files_per_db = {}
+    sorted_files = get_sorted_files()
+    # goes through the sorted os.listdir
+    for filename in sorted_files:
         if '+' not in filename:
             continue
+        # TODO: make the prefix configurable
         key_name = filename.split('+')[0]
-        # if the name is not in the dictionary, create a key with an empty list as value
-        if key_name not in d:
-            d[key_name] = []
-        d[key_name].append(filename)
+        # if key doesn't exist yet, creates a key with an empty list
+        if key_name not in files_per_db:
+            files_per_db[key_name] = []
+        # if key already exists, adds the filename as value
+        files_per_db[key_name].append(filename)
+
+    return files_per_db
 
 
-def parse_date(filename):
-    """ Gets the datetime object out of a filename
+def get_file_date(filename):
+    """ Gets the datetime object from a filename
 
-        Parameters:
-            filename (str) : the filename that contains a datetime object in string
-
-        Returns:
-            parsed_file (datetime object) : the datetime object from filename
-
+    :param filename: the filename of which you want the date to get parsed from
+    :return: file_date: parsed datetime object out of the filename
     """
     try:
+        # TODO: make the prefix configurable
+        # splits the filename from the prefix to the datetime string
         file_to_parse = str(filename).split("+")[1].split(".")[0]
-        parsed_file = datetime.strptime(str(file_to_parse).replace(";", '').replace('%3A', ':'), "%Y-%m-%d%" "H:%M:%S")
+        # gets the datetime string and converts it to a datetime object
+        file_date = datetime.strptime(str(file_to_parse).replace(";", '').replace('%3A', ':'), "%Y-%m-%d%" "H:%M:%S")
     except:
-        print('error with ', filename)
+        print('error with', filename)
         raise
-    return parsed_file
+    return file_date
 
 
-def process_bucket(start_point, list_to_compare, hours):
-    """ Calculates the difference between the starting point of one bucket and the first of the next bucket.
-
-        Parameters:
-            start_point (list) : the first bucket, used as starting point
-            list_to_compare (list) : the bucket that needs to be compared to the start_point
-            hours (float) : the amount of hours there is allowed to be between each back-up, defined in config.toml
-
-        Returns:
-            kill_list (list) : a list of files that will be deleted
-    """
+def create_kill_list(bucket_start: list, bucket_to_compare: list, hours):
     kill_list = []
-    while len(list_to_compare) > 1:
-        for item in list_to_compare:
-            diff_start = parse_date(start_point) - parse_date(item)
+    while len(bucket_to_compare) > 1:
+        for item in bucket_to_compare:
+            diff_start = calc_diff_between_dates(bucket_start, item)
             if diff_start < timedelta(hours=hours):
                 kill_list.append(item)
             else:
@@ -62,113 +68,93 @@ def process_bucket(start_point, list_to_compare, hours):
         if not kill_list:
             return []
         last = kill_list.pop()
-        if last in list_to_compare:
-            start_point = last
-            list_to_compare = list_to_compare[list_to_compare.index(last) + 1:]
+        if last in bucket_to_compare:
+            bucket_start = last
+            bucket_to_compare = bucket_to_compare[bucket_to_compare.index(last) + 1:]
         else:
-            start_point = list_to_compare.pop(0)
+            bucket_start = bucket_to_compare.pop(0)
 
     return kill_list
 
 
-def fill_bucket():
-    """" Fills the buckets with files and adds them to the kill_list
-    """
-    for db_name in d.keys():
+def store_files_in_buckets():
+    files_per_db = get_files_per_db()
+    kill_list = []
+    for db_name in files_per_db.keys():
         this_kill_list = []
         bucket1 = []
         bucket2 = []
         bucket3 = []
         bucket4 = []
         bucket5 = []
-        a = d[db_name][0]
-        for cursor in d[db_name]:
-            diff = parse_date(a) - parse_date(cursor)
-            # first bucket, period in days is defined in config file. standard is 7
+        first_element = files_per_db[db_name][0]
+        for cursor in files_per_db[db_name]:
+            diff = calc_diff_between_dates(first_element, cursor)
             if diff <= timedelta(days=config.get('bucket_first').get('period_in_days')):
-                # appends to the bucket
                 bucket1.append(cursor)
-            # second bucket, period in days is defined in config file. standard is 15
             elif diff < timedelta(days=config.get('bucket_second').get('period_in_days')):
                 bucket2.append(cursor)
-            # third bucket, period in days is defined in config file. standard is 29
             elif diff < timedelta(days=config.get('bucket_third').get('period_in_days')):
                 bucket3.append(cursor)
-            # fourth bucket, period in days is defined in config file. standard is 85
             elif diff < timedelta(days=config.get('bucket_fourth').get('period_in_days')):
                 bucket4.append(cursor)
-            # fifth bucket, period in days is defined in config file. standard is 85
             elif diff >= timedelta(days=config.get('bucket_fifth').get('period_in_days')):
                 bucket5.append(cursor)
 
-        this_kill_list.extend(process_bucket(bucket1[-1], bucket2, config.get('bucket_second').get('hours_between')))
-        this_kill_list.extend(process_bucket(bucket2[-1], bucket3, config.get('bucket_third').get('hours_between')))
-        this_kill_list.extend(process_bucket(bucket3[-1], bucket4, config.get('bucket_fourth').get('hours_between')))
-        this_kill_list.extend(process_bucket(bucket4[-1], bucket5, config.get('bucket_fifth').get('hours_between')))
-        # extends kill_list with every this_kill_list to create one kill_list
+        this_kill_list.extend(create_kill_list(bucket1[-1], bucket2, config.get('bucket_second').get('hours_between')))
+        this_kill_list.extend(create_kill_list(bucket2[-1], bucket3, config.get('bucket_third').get('hours_between')))
+        this_kill_list.extend(create_kill_list(bucket3[-1], bucket4, config.get('bucket_fourth').get('hours_between')))
+        this_kill_list.extend(create_kill_list(bucket4[-1], bucket5, config.get('bucket_fifth').get('hours_between')))
         kill_list.extend(this_kill_list)
-        # prints the amount of files for every database and how many items will be deleted
-        print(db_name, 'files found', len(d[db_name]), '# kill list:', len(this_kill_list))
+
+        print(db_name, 'files found', len(files_per_db[db_name]), '# kill list:', len(this_kill_list))
+
+    return kill_list
 
 
-def get_file_size():
-    """" Returns total size of files in directory
+def kill_list_size(kill_list):
+    kill_list_size = 0
+    for filename in kill_list:
+        file_size = os.path.getsize(f"{config.get('main').get('directory')}\\{filename}")
+        kill_list_size += file_size
+    return kill_list_size
 
-        Returns:
-            total_size(int) : the total size of all files in the given directory
 
-    """
-    total_size = sum(os.path.getsize(f) for f in os.listdir('.') if os.path.isfile(f))
+def total_size_files():
+    total_size = sum(os.path.getsize(f) for f in os.listdir(config.get('main').get('directory')) if os.path.isfile(f))
     return total_size
-    # file_size = os.path.getsize(config.get('main').get('directory'))
-    # print('-------------------------------------------')
-    # print('total file size:', float(file_size * 0.000001), 'MB')
-    # print('amount of files that will be deleted:', len(kill_list))
 
 
-def p_file_size():
-    """" Prints the file size of the result of get_file_size()
-    """
-    print('-------------------------------------------')
-    print('total file size: ', round(float(get_file_size() * 0.000001), 3), 'MB')
-    # print('delete file size: ', round(float(get_del_file_size() * 0.000001), 3), 'MB')
+def confirm_delete(kill_list):
+    delete_file_size = round(float(kill_list_size(kill_list) * 0.000001), 3)
+    total_size = round(float(total_size_files() * 0.000001), 3)
+    print(f'\ntotal file size of files in directory: {total_size} MB')
+    print(f'# kill list file size: {delete_file_size} MB')
+    confirm = input("\nAre you sure you want to delete these files? (y/n) ")
+    if confirm == 'y':
+        delete_files(kill_list)
+    if confirm == 'n':
+        print('Ok.')
+    else:
+        confirm_delete(kill_list)
 
 
-def delete_files():
-    """" Deletes the files in kill_list
-    """
-    my_dir = config.get('main').get('directory')
-    # del_list = []
-    for filename in os.listdir(my_dir):
+def delete_files(kill_list):
+    file_dir = config.get('main').get('directory')
+    for filename in os.listdir(file_dir):
         if filename in kill_list:
-            os.remove(os.path.join(my_dir, filename))
-            print('[', parse_date(filename), ']', filename, 'deleted.')
-        # keep_list = [_ for _ in d[db_name] if _ not in kill_list]
-        # # print(keep_list)
-        # for idx, filename in enumerate(keep_list[:-1]):
-        #     print(idx, parse_date(filename) - parse_date(keep_list[idx + 1]), filename)
-
-
-def safety_measure():
-    """" Prevents deleting files before confirmation
-    """
-    control = input("Would you like to delete these files? (y/n): ")
-    if not control == 'y':
-        if not control == 'n':
-            print('Invalid. (y/n)')
-            safety_measure()
-    if control == 'n':
-        exit()
-    if control == 'y':
-        delete_files()
+            try:
+                os.remove(os.path.join(file_dir, filename))
+                print(filename, 'removed')
+            except OSError:
+                raise OSError
+    print(f'files have been deleted succesfully')
+    exit()
 
 
 if __name__ == '__main__':
     config = toml.load('config.toml')
-    d = {}
-
-    file_sorter()
-    kill_list = []
-    fill_bucket()
-    p_file_size()
-    safety_measure()
+    sorted_files = get_sorted_files()
+    kill_list = store_files_in_buckets()
+    # # cProfile.run('store_files_in_buckets()', sort='time')
+    confirm_delete(kill_list)
