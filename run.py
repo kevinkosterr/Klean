c7d5b39acf5507122681e20a41b24767029f9400
33180770 <koster.k2001@gmail.com>
2020-01-28 10:45:20 +0100

    changed up exceptions

--- run.py
+++ run.py
@@ -13,27 +13,33 @@ from Filesystems.B2FS import B2FS
 
 if __name__ == '__main__':
     c = toml.load('config.toml')
+    # the following variables can be configured in the config.toml file
+    # my_dir, bucket_name, key_id, app_id, folder
     my_dir = c.get('LocalFS').get('directory')
     bucket_name = c.get('B2Blaze').get('bucket')
     key_id = c.get('B2Blaze').get('key_id')
     app_id = c.get('B2Blaze').get('app_id')
     folder = c.get('B2Blaze').get('folder')
+    skip = False
+    # skips the confirmation if -y is passed into the commandline
     if '-y' in sys.argv:
         skip = True
         try:
-            fs = B2FS(bucket_name, key_id, app_id)
-        except:
             fs = B2FS(bucket_name, key_id, app_id, folder)
-    elif len(sys.argv) < 2:
-        print("Choose a filesystem: LocalFS | B2FS")
-        fs = input("")
-        if fs == 'LocalFS':
-            fs = LocalFS(my_dir)
-        if fs == 'B2FS':
-            try:
-                fs = B2FS(bucket_name, key_id, app_id)
-            except:
-                fs = B2FS(bucket_name, key_id, app_id, folder)
+        # if the folder is not found, try it without the folder
+        except:
+            fs = B2FS(bucket_name, key_id, app_id)
+    if not skip:
+        if len(sys.argv) < 2:
+            print("Choose a filesystem: LocalFS | B2FS")
+            fs = input("")
+            if fs == 'LocalFS':
+                fs = LocalFS(my_dir)
+            if fs == 'B2FS':
+                try:
+                    fs = B2FS(bucket_name, key_id, app_id)
+                except:
+                    fs = B2FS(bucket_name, key_id, app_id, folder)
 
     sorted_files = fs.sorted_files
     kill_list = fs.store_files_in_buckets(fs.files_per_db)
